324e59d23b681ed34372c71a6aa53157a0646690
mettemariehws <mettemhws@gmail.com>
2021-12-02 15:04:56 +0100

    Refactor

---
+++ src/main/java/com/dat20b/demo/repository/LocationRepository.java
@@ -0,0 +1,8 @@
+package com.dat20b.demo.repository;
+
+import com.dat20b.demo.model.location;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LocationRepository extends CrudRepository<location, Integer> {
+
+}
