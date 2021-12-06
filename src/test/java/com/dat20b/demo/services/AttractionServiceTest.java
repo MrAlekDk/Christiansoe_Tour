e8de64d74fcc82db6e547c336069b8f136240948
mettemariehws <mettemhws@gmail.com>
2021-12-06 12:51:09 +0100

    Changes to Attraction

--- src/test/java/com/dat20b/demo/services/AttractionServiceTest.java
+++ src/test/java/com/dat20b/demo/services/AttractionServiceTest.java
@@ -9,13 +9,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import reactor.core.Scannable;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.List;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.times;
