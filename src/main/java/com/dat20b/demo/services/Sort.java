d63703561286029bfdc8798a522fd017c45213dc
Alexander <alex030t@stud.kea.dk>
2021-12-09 15:05:28 +0100

    Created a sort method, that sorts routes based on Interest and departure time

--- src/main/java/com/dat20b/demo/services/Sort.java
+++ src/main/java/com/dat20b/demo/services/Sort.java
@@ -1,29 +1,41 @@
 package com.dat20b.demo.services;
 
 import com.dat20b.demo.model.Attraction;
+import com.dat20b.demo.model.Route;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
-public class Sort {
+public abstract class Sort {
 
-    public Sort() {
-    }
-
-
-    public List<Attraction> sortActiveAttractions(List<Attraction> allAttractions) {
-
-        allAttractions.removeIf(this::checkIfInActiveSeason);
+    public static List<Attraction> sortActiveAttractions(List<Attraction> allAttractions) {
+        allAttractions.removeIf(attraction -> checkIfInActiveSeason(attraction));
         return allAttractions;
     }
 
     //Check if current month is between the attraction's starting and ending month
-    private boolean checkIfInActiveSeason(Attraction attraction) {
+    private static boolean checkIfInActiveSeason(Attraction attraction) {
         Month monthNow = LocalDate.now().getMonth();
         Month monthStart = attraction.getActiveSeasonStart().getMonth();
         Month monthEnd = attraction.getActiveSeasonEnd().getMonth();
 
         return monthNow.getValue() >= monthStart.getValue() && monthNow.getValue() <= monthEnd.getValue();
+    }
+
+
+    public static List<Route> sortRoutesByInterestAndDepartureTime(List<Route> routes, String interest, int departureTime) {
+        routes.removeIf(route -> checkInterestAndDepatureTime(route,interest,departureTime));
+        return routes;
+    }
+
+    private static boolean checkInterestAndDepatureTime(Route route, String interest, int depTime){
+        if(route.getTimeDuration().getSeconds() <= Duration.ofMinutes(depTime).getSeconds() && interest.equalsIgnoreCase(route.getInterest())){
+            return false;
+        }
+        else{
+            return true;
+        }
     }
 }
--- src/test/java/com/dat20b/demo/services/SortTestV2.java
+++ src/test/java/com/dat20b/demo/services/SortTestV2.java
@@ -1,8 +1,6 @@
 package com.dat20b.demo.services;
 
-import com.dat20b.demo.model.Attraction;
-import com.dat20b.demo.model.Coordinates;
-import com.dat20b.demo.model.Location;
+import com.dat20b.demo.model.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -17,12 +15,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class SortTestV2 {
 
     List<Attraction> allAttractions = new ArrayList<>();
+    List<Route> allRoutes = new ArrayList<>();
 
-    Sort sort = new Sort();
 
     @BeforeEach
     void setUp() {
-
         Attraction at1 = new Attraction(1, "Attraktion1", "Beskrivelse",
                 Duration.ofSeconds(21000L),
                 LocalDate.of(2021, 1, 1),
@@ -45,6 +42,16 @@ class SortTestV2 {
         allAttractions.add(at1);
         allAttractions.add(at2);
         allAttractions.add(at3);
+
+        Route routeTest1 = new Route("History","A description",Interest.HISTORY.name(), Duration.ofMinutes(45),1000.0);
+        Route routeTest2 = new Route("History","A description",Interest.HISTORY.name(),Duration.ofMinutes(15),1000.0);
+        Route routeTest3 = new Route("History","A description",Interest.ANIMALS.name(),Duration.ofMinutes(40),1000.0);
+        Route routeTest4 = new Route("History","A description",Interest.HISTORY.name(),Duration.ofMinutes(100),1000.0);
+
+        allRoutes.add(routeTest1);
+        allRoutes.add(routeTest2);
+        allRoutes.add(routeTest3);
+        allRoutes.add(routeTest4);
     }
 
     @Test
@@ -52,7 +59,20 @@ class SortTestV2 {
 
         List<Attraction> attractionsToSort = allAttractions;
 
-        List<Attraction> sortedList = sort.sortActiveAttractions(attractionsToSort);
+        List<Attraction> sortedList = Sort.sortActiveAttractions(attractionsToSort);
+
+        assertEquals(2,sortedList.size());
+    }
+
+    @Test
+    void sortRoutesByInterestAndDepartureTimeTest(){
+        //Arrange
+        String interest = Interest.HISTORY.name();
+
+        //Departure time in minutes
+        int departureTime = 46;
+
+        List<Route> sortedList = Sort.sortRoutesByInterestAndDepartureTime(allRoutes,interest,departureTime);
 
         assertEquals(2,sortedList.size());
     }
